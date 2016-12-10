af9d56de96bc052bb96d829f6dd63926a4d1e2bd
spen428 <spen428@users.noreply.github.com>
2016-12-10 07:23:58 +0000

    added Note class and parsing method

--- event.cpp
+++ event.cpp
@@ -1,37 +1,26 @@
+#include <iostream>
+#include <set>
+
 #include "event.h"
 
 Event::Event(uint32_t time, std::string text):
-	time(time), text(text)
-{
-}
+	time(time), text(text){}
 
-Event::~Event()
-{
-}
+Event::~Event(){}
 
 SyncTrackEvent::SyncTrackEvent(uint32_t time, std::string type,
 		uint32_t value):
-	Event(time, ""), type(type), value(value)
-{
-}
+	Event(time, ""), type(type), value(value){}
 
-SyncTrackEvent::~SyncTrackEvent()
-{
-}
+SyncTrackEvent::~SyncTrackEvent(){}
 
 NoteEvent::NoteEvent(uint32_t time, std::string text):
-	Event(time, text), value(0), duration(0)
-{
-}
+	Event(time, text), value(0), duration(0){}
 
 NoteEvent::NoteEvent(uint32_t time, uint32_t value, uint32_t duration):
-	Event(time, ""), value(value), duration(duration)
-{
-}
+	Event(time, ""), value(value), duration(duration){}
 
-NoteEvent::~NoteEvent()
-{
-}
+NoteEvent::~NoteEvent(){}
 
 bool NoteEvent::isEvent()
 {
@@ -45,10 +34,48 @@ bool NoteEvent::isNote()
 
 bool NoteEvent::isFlag()
 {
-	return isNote() && value >= NUM_PLAYABLE_NOTES;
+	// Tap flag is the first non-playable note value
+	return isNote() && value >= TAP_FLAG_VAL;
 }
 
-bool NoteEvent::isPlayable()
+Note::Note(){}
+Note::~Note(){}
+
+bool Note::isTap()
 {
-	return isNote() && value < NUM_PLAYABLE_NOTES;
+	return (value & (1 << TAP_FLAG_VAL));
+}
+
+bool Note::isForce()
+{
+	return (value & (1 << HOPO_FLIP_FLAG_VAL));
+}
+
+void Note::parse_notes(map<uint32_t, Note>& noteMap, vector<NoteEvent>& simultaneousNoteEvents)
+{
+	Note note;
+	note.time = simultaneousNoteEvents[0].time;
+	note.duration = simultaneousNoteEvents[0].duration;
+	note.value = 0;
+
+	set<uint32_t> durationSet;
+
+	// Build note bits
+	for (NoteEvent evt: simultaneousNoteEvents) {
+		if (!evt.isNote())
+			continue;
+		if (!evt.isFlag())
+			durationSet.insert(evt.time);
+		note.value |= (1 << evt.value);
+	}
+
+	// Assert note durations are equal
+	if (!durationSet.size() == 1) {
+		// Durations are not equal, must be an extended sustain. Let's fix it
+		cerr << "Unequal note durations detected at time " << note.time << endl;
+		// TODO
+	}
+
+	// Finally, add note to map
+	noteMap[note.time] = note;
 }
--- event.h
+++ event.h
@@ -2,51 +2,70 @@
 
 #include <string>
 #include <stdint.h>
+#include <map>
+#include <vector>
 
-#define NUM_PLAYABLE_NOTES 5
+#define TAP_FLAG_VAL 5
+#define HOPO_FLIP_FLAG_VAL 6
+
+using namespace std;
 
 class Event
 {
 	public:
-		Event(uint32_t time, std::string text);
+		Event(uint32_t time, string text);
 		~Event();
+
 		uint32_t time;
 		/**
 		 * The text that appears after the "E " in the event
 		 */
-		std::string text;
+		string text;
 };
 
 class SyncTrackEvent: public Event
 {
 	public:
-		SyncTrackEvent(uint32_t time, std::string type, uint32_t value);
+		SyncTrackEvent(uint32_t time, string type, uint32_t value);
 		~SyncTrackEvent();
 		bool isTsChange();
 		bool isTempoChange();
-		std::string type;
+
+		string type;
 		uint32_t value;
 };
-
 
 class NoteEvent: public Event
 {
 	public:
-		NoteEvent(uint32_t time, std::string text);
+		NoteEvent(uint32_t time, string text);
 		NoteEvent(uint32_t time, uint32_t value, uint32_t duration);
 		~NoteEvent();
-		/**
-		 * Returns true if this Note object is a playable note, i.e. is
-		 * a Green, Red, Yellow, Blue, or Orange note.
-		 */
-		bool isPlayable();
-		/**
-		 * Returns true if this Note object is a note flag, i.e. the tap
-		 * flag or the HOPO flag.
-		 */
-		bool isFlag();
 		bool isNote();
+		bool isFlag();
 		bool isEvent();
+
+		/**
+		 * Note value as decimal
+		 */
+		uint32_t value;
+		uint32_t duration;
+};
+
+class Note
+{
+	public:
+		Note();
+		~Note();
+		bool isTap();
+		bool isForce();
+
+		static void parse_notes(map<uint32_t, Note>& noteMap, vector<NoteEvent>& simultaneousNoteEvents);
+
+		uint32_t time;
+		/**
+		 * Note values as bits
+		 */
 		uint32_t value;
 		uint32_t duration;
 };
