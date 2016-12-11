d8de18257cb71c691cb7a698a05a329a53ec433e
spen428 <spen428@users.noreply.github.com>
2016-12-11 14:22:12 +0000

    Added test and main builds to compile script

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-g++ -g -std=c++11 test.cpp chart.cpp event.cpp fix.cpp
+g++ -g -std=c++11 -o test test.cpp chart.cpp event.cpp fix.cpp
+g++ -g -std=c++11 -o main main.cpp chart.cpp event.cpp fix.cpp
